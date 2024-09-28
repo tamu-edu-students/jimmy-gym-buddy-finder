4f45dafb4adc59e5f125fef937408fef6793577d
YashPhatak <yashsameerphatak@gmail.com>
2024-09-28 15:51:33 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
-end
+    validates :name, presence: true
+    validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :gender, presence: true
+  end
