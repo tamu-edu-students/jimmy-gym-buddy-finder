0dda73992cf5ad6078a7c3d7fa881fc281e4b185
YashPhatak <yashsameerphatak@gmail.com>
2024-09-30 20:42:28 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
-    validates :gender, presence: true
-  end
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 3 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 16 }
+  validates :gender, presence: true
+end
