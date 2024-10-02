4f5e446ced6bf0bc62f10e9da546ca39e746bb84
YashPhatak <yashsameerphatak@gmail.com>
2024-10-02 16:16:31 -0500

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,22 @@ class UsersController < ApplicationController
     def new
       @user = User.new # Initializes a new user instance for the form
     end
-  
+
     def create
       @user = User.new(user_params) # Create a new user with the submitted params
-  
+
       if @user.save
-        flash[:notice] = 'User registered successfully.'
+        flash[:notice] = "User registered successfully."
         redirect_to new_user_path
       else
-        flash.now[:alert] = 'There were errors while saving the user.' # Correctly using flash.now
+        flash.now[:alert] = "There were errors while saving the user."
         render :new # This re-renders the new form
       end
     end
-  
+
     private
-  
+
     def user_params
       params.require(:user).permit(:name, :age, :gender) # Strong parameters to prevent mass assignment issues
     end
-  end
-  
+end
