594336d358116d8b7b2f4e47a4eaed997a188e63
Kushal Lahoti <lahotikn18.comp@coep.ac.in>
2024-10-02 17:04:26 -0500

    Resolves issues with migration conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_02_193013) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_02_215832) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
