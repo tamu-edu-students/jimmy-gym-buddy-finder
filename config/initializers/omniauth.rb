c32372a906e8e8880466fb0d956885c6b3c93402
Kushal Lahoti <lahotikn18.comp@coep.ac.in>
2024-10-04 18:36:45 -0500

    Reverts updates in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,8 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       scope: "email, profile", # This grants access to the user's email and profile information.
       prompt: "select_account", # This allows users to choose the account they want to log in with.
       image_aspect_ratio: "square", # Ensures the profile picture is a square.
-      image_size: 50, # Sets the profile picture size to 50x50 pixels.
-      redirect_uri: "#{ENV['APP_URL']}/auth/google_oauth2/callback"
+      image_size: 50 # Sets the profile picture size to 50x50 pixels.
     }
 end
 
