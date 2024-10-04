44a1834d621a7f49f0b5a7d023fd4dd3c2517530
Kushal Lahoti <lahotikn18.comp@coep.ac.in>
2024-10-04 17:52:34 -0500

    Working version - deployment

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+# require "test_helper"
 
-class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class DashboardControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
