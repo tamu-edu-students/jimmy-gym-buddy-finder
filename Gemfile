fd33e6873c646fcfd8ebd43b4e7c9aa10a94ce82
Barry0304 <barry89130663@gmail.com>
2024-10-14 00:57:35 -0500

    omniauth test done

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ group :development, :test do
   gem "rubocop-rails-omakase", require: false
 
   gem "carrierwave", "~> 2.0"
+
+  gem "factory_bot_rails"
 end
 
 group :development do
@@ -76,5 +78,9 @@ group :test do
   gem "rspec-rails"
   gem "simplecov", require: false
   gem "ZenTest"
+  gem "database_cleaner-active_record"
+  gem "rack_session_access"
+  gem "omniauth-test"
+
   # gem "codeclimate-test-reporter", require: nil
 end
---
+++ features/step_definitions/sessions_steps.rb
@@ -0,0 +1,58 @@
+Given("I am not logged in") do
+  page.set_rack_session(user_id: nil)
+end
+
+Given("I am logged in") do
+  @user = FactoryBot.create(:user, :complete_profile)
+  page.set_rack_session(user_id: @user.id)
+end
+
+Given('I am logged in as an non-configured user') do
+  @user = FactoryBot.create(:user, :incomplete_profile)
+  page.set_rack_session(user_id: @user.id)
+end
+
+Given('I am a well-configured user and not log in') do
+  @user = FactoryBot.create(:user, :complete_profile)
+  step "I am not logged in"
+end
+
+Given('I am a non-configured user and not log in') do
+  @user = FactoryBot.create(:user, :incomplete_profile)
+end
+
+When('the authentication fails') do
+  OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+end
+
+When('I click {string}') do |link_text|
+  click_link link_text
+end
+
+When("I click the {string} button") do |button_text|
+  click_button button_text
+end
+
+When('I am on the welcome page') do
+  visit welcome_path
+end
+
+When('I am on my dashboard page') do
+  visit dashboard_user_path(@user)
+end
+
+Then('I should see {string}') do |message|
+  expect(page).to have_content(message)
+end
+
+Then("I should be redirected to my dashboard") do
+  expect(page).to have_current_path(dashboard_user_path(@user))
+end
+
+Then("I should be redirected to edit profile page") do
+  expect(page).to have_current_path(edit_user_path(@user))
+end
+
+Then("I should be redirected to the welcome page") do
+  expect(page).to have_current_path(welcome_path)
+end
