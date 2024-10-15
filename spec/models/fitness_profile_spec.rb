fb3f104150bd0186af9ff7fa9b9b49979ad5b1c2
wcc1101 <davidc1101@gmail.com>
2024-10-15 11:49:45 -0500

    finish rspec test for fitness profile management

---
+++ spec/models/fitness_profile_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe FitnessProfile, type: :model do
+  let(:user) { create(:user) }
+
+  it 'is valid with valid attributes' do
+    fitness_profile = FitnessProfile.new(
+      fitness_goals: 'Lose weight',
+      workout_types: 'Running',
+      gender: 'Male',
+      age_range_start: '18',
+      age_range_end: '28',
+      user: user
+    )
+    expect(fitness_profile).to be_valid
+  end
+
+  it 'is not valid without a fitness goal' do
+    fitness_profile = FitnessProfile.new(fitness_goals: nil, user: user)
+    expect(fitness_profile).not_to be_valid
+  end
+
+  it 'is not valid without a user' do
+    fitness_profile = FitnessProfile.new(fitness_goals: 'Lose weight', user: nil)
+    expect(fitness_profile).not_to be_valid
+  end
+end
