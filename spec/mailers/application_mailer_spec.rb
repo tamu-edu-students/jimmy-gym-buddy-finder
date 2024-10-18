147fd685d192e1556b10dec3ddaebbb334897cdd
Mrunmay Deshmukh <mrunmayd@tamu.edu>
2024-10-17 20:47:49 -0500

    rspec coverage

--- spec/mailers/application_mailer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationMailer, type: :mailer do
-  describe '#welcome_email' do
-    it 'sends the welcome email' do
-      # No implementation
-    end
-  end
-end
